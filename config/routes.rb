61596fd44c6f2b780ea98feaa5b1f6cc9778bc8a
Linda Mollinedo <lindamse@gmail.com>
2019-10-25 16:24:50 -0300

    Cocktails blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :create, :show]
-
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+    resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 end
